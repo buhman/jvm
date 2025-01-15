198c7637f7a01b037f0eebf20c049f6c92086af1
Zack Buhman <zack@buhman.org>
2025-01-14 22:10:46 -0600

    doc: add howto

--- doc/build.sh
+++ doc/build.sh
@@ -1,7 +1,11 @@
-make4ht entry.tex "pic-m,pic-equation,svg"
+set -eux
 
-mv entry.html index.html
-mv entry.css index.css
+make4ht $1 "pic-m,pic-equation,svg"
+
+css="$(basename ${1%.tex}.css)"
+
+mv "$(basename ${1%.tex}.html)" index.html
+mv "${css}" index.css
 
 echo 'img[alt="PIC"] { width: 100%; }' >> index.css
 echo '.cmtt-10 { font-size: 0.9em; }' >> index.css
@@ -9,7 +13,8 @@ echo 'img[src="index3x.svg"] { height: 2.5em; }' >> index.css
 echo 'object[class="graphics"] { width: 100% }' >> index.css
 
 sed -i '/prefers-color-scheme/d' index.css
-sed -i 's| </span>|</span> |g' index.html
-sed -i 's/entry.css/index.css/g' index.html
 sed -i 's/1.5157em/1.3157em/g' index.css
 sed -i 's/1.3195em/1.0195em/g' index.css
+
+sed -i 's| </span>|</span> |g' index.html
+sed -i "s/${css}/index.css/g" index.html
